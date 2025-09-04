ce9cd49961c1acfab977cd7a0ed736c81eab7926
jai siya ram <jiya17670@gmail.com>
2025-09-04 18:46:32 +0530

    Update root route to render listing index page

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.use("/Listings",listingsRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 app.get("/",(req,res)=>{
-    res.render("listing/index");
+    res.render("listings/index");
   });
 
   
